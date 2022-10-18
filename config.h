8a19b33b1741501aeb03f6da3a52a7fb4d1934f0
Bruno <bruno.vieirarosas@heig-vd.ch>
2022-10-18 15:52:05 +0200

    Initial commit

---
+++ config.h
@@ -0,0 +1,12 @@
+/*
+ *  HEIG-VD
+ *  Description: Configuration file
+ *  Author(s): Bruno Vieira
+ *  Date: 18.10.2022
+*/
+
+const int BOARD_HEIGHT = 6;
+const int BOARD_LENGTH = 7;
+const int EMPTY = 0;
+const int PLAYER1 = 1;
+const int PLAYER2 = 2;
--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
+/*
+ *  HEIG-VD
+ *  Description: PRG1 Labo3 - Puissance 4
+ *  Author(s): Bruno Vieira
+ *  Date: 18.10.2022
+*/
+
 #include <iostream>
+#include "config.h"
+
+using namespace std;
+
+int main(){
+    int board[BOARD_HEIGHT][BOARD_LENGTH] = {EMPTY};
+    // print board for testing
+    /*for (int i = 0; i < BOARD_HEIGHT; ++i) {
+        for (int j = 0; j < BOARD_LENGTH; ++j) {
+            cout << board[i][j] << " ";
+        }
+        cout<<"\n";*/
+    return 0;
+}
