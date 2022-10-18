601faa2e3c2abfd8b87553afa248cfb06d6b12d7
Davtek11 <114155001+Davtek11@users.noreply.github.com>
2022-10-18 16:28:57 +0200

    falldown + draw

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 /*
  *  HEIG-VD
  *  Description: PRG1 Labo3 - Puissance 4
- *  Author(s): Bruno Vieira
+ *  Author(s): Bruno Vieira - David Berger
  *  Date: 18.10.2022
 */
 
@@ -10,13 +10,50 @@
 
 using namespace std;
 
+void drawBoard(int board[BOARD_HEIGHT][BOARD_LENGTH]);
+void fallDown(int board[BOARD_HEIGHT][BOARD_LENGTH], int column, int player);
+
 int main(){
     int board[BOARD_HEIGHT][BOARD_LENGTH] = {EMPTY};
-    // print board for testing
-    /*for (int i = 0; i < BOARD_HEIGHT; ++i) {
-        for (int j = 0; j < BOARD_LENGTH; ++j) {
-            cout << board[i][j] << " ";
-        }
-        cout<<"\n";*/
+    bool winner = false;
+    int columnImput;
+
+    drawBoard(board);
+
+    while (winner == false) {
+
+        cout << "Dans quelle colonne voulez-vous jouer ? ";
+        cin >> columnImput;
+        fallDown(board, columnImput - 1, PLAYER1);
+        drawBoard(board);
+
+
+
+    }
+
     return 0;
+
+}
+
+void fallDown(int board[BOARD_HEIGHT][BOARD_LENGTH], int column, int player) {
+
+    for (int l = BOARD_HEIGHT -1; l > 0; l--) {
+        if (board[l][column] == EMPTY) {
+            board[l][column] = player;
+            break;
+        }
+    }
+
+}
+
+void drawBoard(int board[BOARD_HEIGHT][BOARD_LENGTH]) {
+
+    // print board for testing
+    for (int i = 0; i < BOARD_HEIGHT; ++i) {
+        for (int j = 0; j < BOARD_LENGTH; ++j) {
+            cout << " | " << board[i][j];
+        }
+        cout << " | \n";
+        cout << "  ----------------------------" "\n";
+    }
 }
